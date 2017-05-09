94cdd73c64a6fb19d06cac92e0021ea423f62296
codenamesrk <srijit@outlook.com>
2017-05-09 15:00:14 +0530

    add topbar

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+// Web Font Loader
+window.WebFontConfig = {
+	google: {
+	  families: ['Open+Sans:300,400,600']
+	}
+};
+(function() {
+	var wf = document.createElement('script');
+	wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	wf.type = 'text/javascript';
+	wf.async = 'true';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(wf, s);
+})();
+
 $(document).foundation();

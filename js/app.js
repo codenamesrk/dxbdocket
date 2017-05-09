457f10ca442a13e637c09de5e2de594e9b39cce4
codenamesrk <srijit@outlook.com>
2017-05-09 23:43:16 +0530

    set comments,rating,map and adslot 2 section

--- js/app.js
+++ js/app.js
@@ -14,3 +14,13 @@ window.WebFontConfig = {
 })();
 
 $(document).foundation();
+
+$(".rating").rate();
+
+//or for example
+var options = {
+    max_value: 6,
+    step_size: 0.5,
+}
+
+$(".rating").rate(options);
